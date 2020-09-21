6744c7159f74d9a0f57b9a81f99e0050cfaefb74
Srr233 <Sr233@yandex.ru>
2020-09-21 12:07:25 +0300

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  if (!(options || str)) return false;
+
+  let result = '';
+  let fixOptions = {
+    repeatTimes: 1,
+    separator: "+",
+    addition: "",
+    additionRepeatTimes: 1,
+    additionSeparator: "|"
+  };
+
+  for (let [key, value] of Object.entries(options)) {
+    if (options[key] !== undefined) {
+      fixOptions[key] = options[key];
+    }
+  }
+  for (let i = 0; i < fixOptions.repeatTimes; i++) {
+    result += `${str}`;
+
+
+    for (let i = 0; i < fixOptions.additionRepeatTimes; i++) {
+      result += `${fixOptions.addition}`;
+
+      if (i < fixOptions.additionRepeatTimes - 1) {
+        result += `${fixOptions.additionSeparator}`;
+      }
+    }
+
+    if (i < fixOptions.repeatTimes - 1) result += `${fixOptions.separator}`;
+  }
+  return result;
+}
+
   
