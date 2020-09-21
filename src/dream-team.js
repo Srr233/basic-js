5ef1ceeff778ab948476fb2e515d94c409a63df8
Srr233 <Sr233@yandex.ru>
2020-09-21 12:06:15 +0300

    feat: implement createDreamTeam foo

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let secretTeamNames = [];
+
+  //----------------------
+  for (let name of members) {
+
+    if (typeof name === 'string') {
+      secretTeamNames.push(name.trim()[0].toUpperCase());
+    }
+  }
+  //----------------------
+  return secretTeamNames.sort().join('');
 };
+
