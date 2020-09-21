8042e5e55957d13c556b9d7fdbcf959e85614990
Srr233 <Sr233@yandex.ru>
2020-09-21 12:08:23 +0300

    feat: implement calculateHanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  /*
+  disksNumber - a number of disks
+  turnsSpeed - speed
+  both arguments are number 
+  
+  foo need to return an object with 2 properties
+  * turns - minimum number of turns to solve the puzzle
+  * seconds - minimum number of seconds to solve the puzzle at a given turnsSpeed, 
+  seconds must be an integer, rounded down
+  */
+
+  let turns = 2;
+  for (let i = 1; i < disksNumber; i++) {
+    turns *= 2
+  }
+  turns--;
+  const seconds = Math.floor((3600 / turnsSpeed) * turns);
+  return { turns: turns, seconds: seconds };
 };
+
