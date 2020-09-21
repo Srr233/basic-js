443b240ff90071606d64824e786a109fe5b0b94e
Srr233 <Sr233@yandex.ru>
2020-09-21 12:12:14 +0300

    feat: implement transform foo

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error('isn\'t an array');
+  } else if (!arr.length) {
+    return [];
+  }
+
+  let transformArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next':
+        if (i + 1 < arr.length) transformArray.push(arr[i + 1]);
+        break;
+      case '--discard-next':
+        i++;
+        break;
+      case '--double-prev':
+        if ((i >= 1) && (arr[i - 2] != '--discard-next')) transformArray.push(arr[i - 1]);
+        break;
+      case ('--discard-prev'):
+        if ((i >= 1) && (arr[i - 2] != '--discard-next')) transformArray.splice(-1, 1);
+        break;
+      default:
+        transformArray.push(arr[i]);
+    }
+  }
+  return transformArray;
+}
