d1ecf1995b41547f4b7c9e49bdec748f0a51ded3
Srr233 <Sr233@yandex.ru>
2020-09-21 12:05:11 +0300

    feat: implement countCats foo

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  if (!(backyard[0] && backyard[1])) return 0;
+
+  let howManyCats = backyard.map(back => {
+    return back.reduce((previousValue, currentValue) => {
+      if (currentValue === "^^") {
+        return previousValue += 1;
+      } else {
+        return previousValue;
+      }
+    }, 0);
+  }).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+  return howManyCats;
+}
